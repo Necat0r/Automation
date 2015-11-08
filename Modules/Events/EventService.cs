04c249f801373194e3a8a217cb6fa1aad3014c61
Necat0r <j_lindmark@hotmail.com>
2015-11-08 23:43:52 +0100

    Events service changes
    Event service now take a dictionary of string,string pairs
    Cleaned out old/unused code

--- Modules/Events/EventService.cs
+++ Modules/Events/EventService.cs
@@ -1,4 +1,5 @@
-﻿using Module;
+﻿using Logging;
+using Module;
 using System;
 using System.Collections.Generic;
 
@@ -6,29 +7,24 @@ namespace Events
 {
     public class EventService : ServiceBase
     {
-        public class Event
+        public class Event : EventArgs
         {
             public string Name { get; set; }
-            public object Data { get; set; }
+            public Dictionary<string,string> Data { get; set; }
         }
+
+        public event EventHandler<Event> OnEvent;
 
         public EventService(string name, ServiceCreationInfo info)
             : base("events", info)
-        {
-        }
-
-        //[ServicePutContract()]
-        //public void OnEventRequest(dynamic parameters, dynamic body)
-        //{
-        //    Event requestEvent = DynamicMapper.Map<Event>(body);
-
-        //    Console.WriteLine("Got event with name: " + requestEvent.Name + ", body name: " + body.Name);
-        //}
+        {}
 
         [ServicePutContract()]
         public void OnEventRequest(Event externalEvent)
         {
-
+            Log.Info("Got event " + externalEvent.Name);
+            if (OnEvent != null)
+                OnEvent(this, externalEvent);
         }
     }
 }
