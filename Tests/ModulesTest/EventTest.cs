eeff87b261f245952174e09e48a05ffb7e46b944
Necat0r <j_lindmark@hotmail.com>
2016-10-16 13:28:46 +0200

    Updated tests for EventService

--- Tests/ModulesTest/EventTest.cs
+++ Tests/ModulesTest/EventTest.cs
@@ -1,25 +1,106 @@
-﻿using System;
-using System.Text;
-using System.Collections.Generic;
+﻿using Events;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using ModuleBaseTest;
-using Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 
 namespace ModulesTest
 {
     [TestClass]
     public class EventTest
     {
-        [TestMethod]
-        public void EventService_TestCreation()
+        private EventService CreateEventService()
         {
             var util = new TestUtil();
-
-            string name = "LampTest";
+            string name = "EventService";
             string type = typeof(EventService).AssemblyQualifiedName;
 
             var serviceConfiguration = new Dictionary<string, string>();
-            util.CreateService(name, type, serviceConfiguration);
+
+            return (EventService) util.CreateService(name, type, serviceConfiguration);
+        }
+
+        private EventService.Event CreateTestEvent()
+        {
+            var testEvent = new EventService.Event();
+            testEvent.Name = "testEvent";
+            testEvent.Data.Add("hello", "world");
+
+            return testEvent;
+        }
+
+        private bool AreEqual<K, V>(Dictionary<K, V> lhs, Dictionary<K, V> rhs)
+        {
+            return lhs.Count == rhs.Count &&
+                lhs.Keys.All(k => rhs.ContainsKey(k) && object.Equals(lhs[k], rhs[k]));
+        }
+            
+
+        [TestMethod]
+        public void EventService_TestCreation()
+        {
+            EventService service = CreateEventService();
+            Assert.IsNotNull(service);
+        }
+
+        [TestMethod]
+        public void EventService_TestNull()
+        {
+            var service = CreateEventService();
+
+            service.OnEvent += (sender, serviceEvent) =>
+                {
+                    Assert.Fail();
+                };
+
+            service.OnEventRequest(null);
+        }
+
+        [TestMethod]
+        public void EventService_TestEvent()
+        {
+            var service = CreateEventService();
+
+            var testEvent = CreateTestEvent();
+            var externalEvent = CreateTestEvent();
+
+            bool eventTriggered = false;
+
+            service.OnEvent += (sender, serviceEvent) =>
+                {
+                    Assert.AreEqual(testEvent.Name, serviceEvent.Name);
+                    Assert.IsTrue(AreEqual(testEvent.Data, serviceEvent.Data));
+
+                    eventTriggered = true;
+                };
+
+            service.OnEventRequest(externalEvent);
+
+            Assert.IsTrue(eventTriggered);
+        }
+
+        [TestMethod]
+        public void EventService_TestMultipleListeners()
+        {
+            var service = CreateEventService();
+            var testEvent = CreateTestEvent();
+
+            int eventCount = 0;
+            
+            EventHandler<EventService.Event> handler = (sender, serviceEvent) =>
+                {
+                    ++eventCount;
+                };
+
+            service.OnEvent += handler;
+            service.OnEvent += handler;
+            service.OnEvent += handler;
+
+            service.OnEventRequest(testEvent);
+
+            Assert.AreEqual(3, eventCount);
         }
     }
 }
